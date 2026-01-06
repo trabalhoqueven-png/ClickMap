2e34441d12d0a595690f66ee40365eda9406ce01
Queven <trabalhoqueven@gmail.com>
2026-01-05 22:53:27 -0300

    Update Firebase configuration and imports

--- firebase.js
+++ firebase.js
@@ -4,15 +4,16 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCai7P5axNyYt2g9x9wqsU7DaA_tNZ3Pnc",
-  authDomain: "casa-d5eae.appspot.com",
-  projectId: "casa-d5eae",
-  storageBucket: "casa-d5eae.appspot.com",
-  messagingSenderId: "295692575488",
-  appId: "1:295692575488:web:8a3446d5a58a5e720b89e5"
+  apiKey: "AIzaSyAcA7OgB2HYUFKYb7hthdGS86kLMn1ASBY",
+  authDomain: "clickmap-483501.firebaseapp.com",
+  projectId: "clickmap-483501",
+  storageBucket: "clickmap-483501.firebasestorage.app",
+  messagingSenderId: "879441067528",
+  appId: "1:879441067528:web:426f7fe43a0464acd59bbd"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 export const storage = getStorage(app);
