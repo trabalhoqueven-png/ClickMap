2d625f85611a99bf8e470cd46f36860aa0aad16f
Queven <trabalhoqueven@gmail.com>
2026-01-06 00:34:38 -0300

    Reformat firebase.js for consistency

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
- apiKey: "AIzaSyDxY7bW7ywWgxPRfosKNSl8_2gyzGRQ3eY",
+    apiKey: "AIzaSyDxY7bW7ywWgxPRfosKNSl8_2gyzGRQ3eY",
     authDomain: "clickmap-ae0ca.firebaseapp.com",
     projectId: "clickmap-ae0ca",
     storageBucket: "clickmap-ae0ca.firebasestorage.app",
     messagingSenderId: "388461628722",
-    appId: "1:388461628722:web:425b5089c36a819d625b2b",
+    appId: "1:388461628722:web:425b5089c36a819d625b2b"
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -17,3 +17,4 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 export const storage = getStorage(app);
+
