b4f93293c4c88d2859e8eb4d13533ca6b2808196
Queven <trabalhoqueven@gmail.com>
2026-01-09 07:27:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,9 +12,10 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+  if (event.request.destination === "script") {
+    event.respondWith(fetch(event.request));
+    return;
     })
   );
 });
+
