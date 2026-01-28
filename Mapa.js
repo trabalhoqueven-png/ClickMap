40d81e05a57d1be6ae71049d54bac014eb6ec05f
Queven <trabalhoqueven@gmail.com>
2026-01-28 08:42:51 -0300

    Update Mapa.js

--- Mapa.js
+++ Mapa.js
@@ -24,10 +24,11 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxY7bW7ywWgxPRfosKNSl8_2gyzGRQ3eY",
-  authDomain: "clickmap-ae0ca.firebaseapp.com",
-  projectId: "clickmap-ae0ca"
+  apiKey: "AIzaSyC5TlYdXT6Y0keGZY_DVXPeE9Ru4NqhHzY",
+  authDomain: "qcassino-227ab.firebaseapp.com",
+  projectId: "qcassino-227ab"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -586,6 +587,7 @@ map.whenReady(() => {
   iniciarLocalizacaoTempoReal();
   abrirCasaPorLink(); // 🔥 AQUI
 });
+
 
 
 
