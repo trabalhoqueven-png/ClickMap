c96476d2ebc6b540945807e37f61c4c77de0c091
Queven <trabalhoqueven@gmail.com>
2026-01-28 08:38:51 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxY7bW7ywWgxPRfosKNSl8_2gyzGRQ3eY",
-  authDomain: "clickmap-ae0ca.firebaseapp.com",
-  projectId: "clickmap-ae0ca",
-  storageBucket: "clickmap-ae0ca.firebasestorage.app",
-  messagingSenderId: "388461628722",
-  appId: "1:388461628722:web:425b5089c36a819d625b2b"
+  apiKey: "AIzaSyC5TlYdXT6Y0keGZY_DVXPeE9Ru4NqhHzY",
+  authDomain: "qcassino-227ab.firebaseapp.com",
+  projectId: "qcassino-227ab",
+  storageBucket: "qcassino-227ab.firebasestorage.app",
+  messagingSenderId: "575212961704",
+  appId: "1:575212961704:web:49ff445136e10f025fa79d"
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -17,3 +17,4 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 export const storage = getStorage(app);
+
