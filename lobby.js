a3cc501078b5b3106582e3ba8e5e22804a5c30b5
Queven <trabalhoqueven@gmail.com>
2026-01-28 18:30:11 -0300

    Update lobby.js

--- lobby.js
+++ lobby.js
@@ -1,20 +1,23 @@
 import { auth, db } from "./firebase.js";
-import {
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-import {
-  doc, getDoc
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const saldoEl = document.getElementById("saldo");
 const sairBtn = document.getElementById("sair");
 
 onAuthStateChanged(auth, async user => {
-  if (!user) return location.href = "index.html";
+  if (!user) {
+    location.replace("index.html");
+    return;
+  }
 
   const snap = await getDoc(doc(db, "usuarios", user.uid));
+
+  if (!snap.exists()) {
+    saldoEl.innerText = "0";
+    return;
+  }
+
   saldoEl.innerText = snap.data().credito;
 });
 
@@ -26,5 +29,6 @@ history.pushState(null, "", location.href);
 window.onpopstate = () => {
   history.pushState(null, "", location.href);
 };
+
 
 
