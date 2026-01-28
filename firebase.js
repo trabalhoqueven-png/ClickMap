3b599b9c94b91a3e3c51ad34bab74ab408f82422
Queven <trabalhoqueven@gmail.com>
2026-01-28 11:16:53 -0300

    Refactor firebase.js to simplify firebaseConfig
    Removed storage and messagingSenderId from firebaseConfig.

--- firebase.js
+++ firebase.js
@@ -1,20 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5TlYdXT6Y0keGZY_DVXPeE9Ru4NqhHzY",
+  apiKey:"AIzaSyC5TlYdXT6Y0keGZY_DVXPeE9Ru4NqhHzY",
   authDomain: "qcassino-227ab.firebaseapp.com",
-  projectId: "qcassino-227ab",
-  storageBucket: "qcassino-227ab.firebasestorage.app",
-  messagingSenderId: "575212961704",
-  appId: "1:575212961704:web:49ff445136e10f025fa79d"
+  projectId: "qcassino-227ab"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-export const storage = getStorage(app);
 
