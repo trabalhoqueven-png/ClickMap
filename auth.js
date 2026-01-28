aa8d7f382399ca98a454055d57bcd333fd17651e
Queven <trabalhoqueven@gmail.com>
2026-01-28 11:35:11 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,16 +2,42 @@ import { auth, db } from "./firebase.js";
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 import {
-  doc, setDoc, serverTimestamp
+  doc, setDoc
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-onAuthStateChanged(auth, user => {
-  if (user && user.emailVerified) {
-    location.href = "Mapa.html";
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const msg = document.getElementById("msg");
+
+window.login = async () => {
+  try {
+    await signInWithEmailAndPassword(auth, email.value, senha.value);
+    location.href = "lobby.html";
+  } catch {
+    msg.innerText = "Erro no login";
   }
+};
+
+window.cadastrar = async () => {
+  try {
+    const cred = await createUserWithEmailAndPassword(
+      auth, email.value, senha.value
+    );
+
+    await setDoc(doc(db, "usuarios", cred.user.uid), {
+      credito: 10
+    });
+
+    location.href = "lobby.html";
+  } catch {
+    msg.innerText = "Erro no cadastro";
+  }
+};
+
+onAuthStateChanged(auth, user => {
+  if (user) location.href = "lobby.html";
 });
